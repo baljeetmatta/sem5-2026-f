12c4130df6eb9a87602d5aaa097c5d47c3664a6c
Baljeet <baljeet@Baljeets-MacBook-Air.local>
2026-06-24 12:17:26 +0530

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,112 @@
+//alert("Hello");
+// a=10;
+// //alert(a);
+// console.log(a,typeof(a));
+// a="test";
+// console.log(a,typeof(a));
+"use strict"
+// var code;
+
+
+// code="test";
+
+
+
+
+
+
+
+
+
+// code="Programming"
+
+
+
+
+
+
+
+
+// // console.log(code);
+// console.log(code);
+
+// var code="ABC";
+// console.log(code);
+// function test()
+// {  
+//     console.log(a);
+//     var a=10;
+//     console.log(a);
+
+// }
+
+
+
+// test();
+// console.log(a);
+
+
+// var a=10;
+
+// console.log(a);
+
+// var a=20;
+
+//const a=10;
+//a=20;
+
+// let obj={
+//     name:"test"
+// }
+// console.log(obj);
+// obj.name="Code";
+// console.log(obj);
+// const data=obj;
+// data.name="Another";
+// console.log(data);
+// data=10;
+
+//Data type Primitive /non primitiv
+//undefined, null
+// let a="2125";
+// let b=Number(a)+1;
+// console.log(true+7);
+// console.log(Number(false))
+
+
+
+
+// a+b
+// N+N=sum;
+// S+N=concat
+
+// function display(){
+
+//     console.log("Funcation called");
+// }
+// display();
+// function sum(a,b=0)
+// {
+//     // if(b==undefined)
+//     //     b=0;
+//     console.log(a+b);
+
+// }
+// sum(12,20)
+
+
+function sum()
+{
+   // console.log(arguments);
+   let s=0;
+    for(let i=0;i<arguments.length;i++)
+        s+=arguments[i]
+
+    console.log(s);
+
+
+
+}
+sum(1,2,3)
+sum(3,4)
+sum(7,4,5,6,8)
