eff52de66e12ccc75ac55434d9ba6a895b777330
Baljeet <baljeet@Baljeets-MacBook-Air.local>
2026-07-09 12:11:59 +0530

    promises, ajax

--- assets/js/async.js
+++ assets/js/async.js
@@ -20,3 +20,79 @@ function increment()
 
 
 }
+
+//
+let number=16;
+const evenPromise=new Promise((resolve,reject)=>{
+
+    setTimeout(()=>{if(number%2==0)
+        resolve({data:number,msg:'test'});
+    else
+        reject(number);
+},1000)
+    
+
+})
+
+// //console.log(evenPromise);
+// evenPromise.then((data)=>{
+//     console.log("Its an even",data)
+
+// }).catch((data)=>{
+
+//     console.log("Its not an even",data);
+
+// })
+// const oddPromise=new Promise((resolve,reject)=>{
+//     reject();
+
+// })
+
+// // Promise.allSettled([evenPromise,oddPromise]).then((response)=>{
+// //     console.log(response);
+
+// // })
+
+// // Promise.any([evenPromise,oddPromise]).then((response)=>{
+// //     console.log(response);
+    
+// // })
+// let promiseOne=new Promise((resolve,reject)=>{
+// setTimeout(()=>{resolve()},300);
+
+
+// })
+
+// let promiseTwo=new Promise((resolve,reject)=>{
+// setTimeout(()=>{reject()},500);
+
+
+// })
+// Promise.race([promiseOne,promiseTwo]).then(()=>{
+
+//     console.log("Resolved");
+    
+// }).catch(()=>{
+//     console.log("Rejected");
+// })
+
+//1. Fetch->then->Structured data(readableStream)
+fetch("https://jsonplaceholder.typicode.com/posts",{
+    method:"GET",
+    body:JSON.stringify(),
+    headers:{
+        "content-type":"application/json"
+    }
+
+}).then((data)=>{
+    console.log(data);
+  return  data.json();//promise//json,text ->Promise
+
+
+}).then((response)=>{ //ReadableStream->json
+
+    console.log(response);
+})
+
+
+
